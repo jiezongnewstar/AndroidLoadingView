8b0ccb2ddbe0e9edc654cd666ac9ee483e3fe884
jiajie <jia@123>
2017-08-01 14:11:57 +0800

    mainifest 冲突解决

--- app/src/main/java/com/jiajie/loadingview/MainActivity.java
+++ app/src/main/java/com/jiajie/loadingview/MainActivity.java
@@ -1,12 +1,12 @@
 package com.jiajie.loadingview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.jiajie.load.LoadingDialog;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
